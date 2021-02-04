d81ab8f010263e178f9fb8ffa0c80130e33cc084
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-02-05 01:41:32 +0530

    styled audio component

--- src/components/Definations/Definations.js
+++ src/components/Definations/Definations.js
@@ -7,7 +7,7 @@ const Definations = ({word, category , meanings, lightMode}) => {
             {
                 meanings[0] && word && category=== 'en' && (
                     <audio src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio} 
-                    style={{backgroundColor:"#fff", borderRadius:10}}
+                    style={{backgroundColor:"#fff", borderRadius:10 , width:"100%"}}
                      controls>
                         Your Browser doesn't support audio element.
                     </audio>
